7d34473677a7d343125cae68a70b9bfe68f94c71
ChandanSingh-vns <chandanvns.singh12@gmail.com>
2017-12-08 13:22:54 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,80 @@
+<footer>
+		<!--<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="widget">
+						<h5 class="widgetheading">Nam libero tempore</h5>						
+						<p>soluta nobis est eligendi optio cumque nihil impedit quo minus id
+						excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
+						
+						</p>						
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="widget">
+						<h5 class="widgetheading">Pages</h5>
+						<ul class="link-list">
+							<li><a href="#">Press release</a></li>
+							<li><a href="#">Terms and conditions</a></li>
+							<li><a href="#">Privacy policy</a></li>
+							<li><a href="#">Career center</a></li>
+							<li><a href="#">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="widget">
+						<h5 class="widgetheading">Latest posts</h5>
+						<ul class="link-list">
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Pellentesque et pulvinar enim</a></li>
+							<li><a href="#">Natus error sit voluptatem </a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>-->
+				
+		<div class="sub-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<ul class="social-network">
+							<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest fa-1x"></i></a></li>
+							<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus fa-1x"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-lg-12">
+						<div class="copyright">
+							<p>
+								<span>&copy;AKR Education 2017 All right reserved. By </span><a href="http://www.akreducation.com" target="_blank">AKR Education</a>
+							</p>
+                            
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--end footer-->
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/responsive-slider.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script>
+	wow = new WOW(
+	 {
+	
+		}	) 
+		.init();
+	</script>
+  </body>
+</html>
