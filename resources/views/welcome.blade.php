3b53c11d64a10f5c0d8e1e6a5d5eb9609519a7c1
chandansingh <chandanvns.singh12@gmail.com>
2017-12-12 22:13:58 +0530

    jkjk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - Develop branch
+                    Laravel - Develop branch change
                 </div>
 
                 <div class="links">
