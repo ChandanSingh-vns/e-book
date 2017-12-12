5cd0213bafd273a0b25ab644e38e6bc5c57316f1
chandansingh <chandanvns.singh12@gmail.com>
2017-12-12 22:04:16 +0530

    change header label

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - Develop branch
                 </div>
 
                 <div class="links">
